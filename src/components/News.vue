4360f26e1c4b563f35d16d9e455ee816c44ce047
点影 <dianyingx007@163.com>
2017-02-16 14:23:21 +0800

    引入jQuery和flexslider

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,7 +1,40 @@
 <template>
-    <div id="news">this is news</div>
+    <div id="news">
+        <div class="flexslider">
+            <ul class="slides">
+                <li class="slide">
+                    <div>img</div>
+                    <h2>title</h2>
+                    <p>detail</p>
+                </li>
+                <li class="slide">
+                    <div>img</div>
+                    <h2>title</h2>
+                    <p>detail</p>
+                </li>
+                <li class="slide">
+                    <div>img</div>
+                    <h2>title</h2>
+                    <p>detail</p>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 <script>
+import '../ignore_lib/jquery.flexslider.min.js'
+import '../ignore_lib/flexslider.css'
+import $ from 'jquery'
+
+export default {
+    name: 'news',
+    mounted () {
+        $('.flexslider').flexslider()
+    }
+}
 </script>
 <style>
+.slide {
+    height: 4rem;
+}
 </style>
